e31585c7f610f0157790071a7c26911ddd44882b
Earl Holland <eholland@paloaltonetworks.com>
2022-11-19 10:09:58 -0600

    add scripts directory

---
+++ scripts/install-twistcli.sh
@@ -0,0 +1,8 @@
+#Example: Download twistcli from Prisma cloud console and install
+curl -k -O -u [access key id]:[secret access key] [console url]/api/v1/util/twistcli;
+
+#Example: install from local zip file
+#unzip scripts/twistcli.zip;
+
+#Make twistclie executable
+chmod a+x twistcli;
---
+++ scripts/scan-image.sh
@@ -0,0 +1,3 @@
+./twistcli images scan --details --address [console url] -u [access key id]:[secret access key] $1
+
+#./twistcli images scan --details --address [console url] -u [access key id]:[secret access key] myacr.azurecr.io/rabbitmq-prisma:prisma
